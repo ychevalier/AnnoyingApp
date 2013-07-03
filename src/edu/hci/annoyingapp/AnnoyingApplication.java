65ec226543a47a746320af843e3aafda660f2add
ychevalier <yann.m.chevalier@gmail.com>
2013-07-03 15:16:37 +0100

    And now imageselector start at boot

--- src/edu/hci/annoyingapp/AnnoyingApplication.java
+++ src/edu/hci/annoyingapp/AnnoyingApplication.java
@@ -1,232 +1,252 @@
 package edu.hci.annoyingapp;
 
+import android.app.AlarmManager;
+import android.app.Application;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
 import java.util.Calendar;
 
 import edu.hci.annoyingapp.services.AnnoyingService;
 import edu.hci.annoyingapp.services.DataSenderService;
-import android.app.AlarmManager;
-import android.app.Application;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
+import edu.hci.annoyingapp.utils.Common;
 
 public class AnnoyingApplication extends Application {
 
-	public static final boolean DEBUG_MODE = true;
+    public static final boolean DEBUG_MODE = false;
 
-	// Little trick so that every instance
-	// of SettingsActivity can access the alarm...
-	private static PendingIntent mDialogIntent;
-	private static PendingIntent mDataIntent;
+    // Little trick so that every instance
+    // of SettingsActivity can access the alarm...
+    private static PendingIntent mDialogIntent;
+    private static PendingIntent mDataIntent;
 
-	private static boolean mIsDialogRunning;
-	
-	public static String[] IMAGES = {
-		"anchor",
-		"ant",
-		"apple",
-		"arm",
-		"arrow",
-		"ashtray",
-		"ball",
-		"balloon",
-		"banana",
-		"bed",
-		"bell",
-		"belt",
-		"bench",
-		"bird",
-		"book",
-		"bottle",
-		"butterfly",
-		"button",
-		"camel",
-		"candle",
-		"car",
-		"carrot",
-		"cat",
-		"caterpillar",
-		"chisel",
-		"cigarette",
-		"clock",
-		"cloud",
-		"comb",
-		"cow",
-		"crown",
-		"desk",
-		"dog",
-		"door",
-		"dress",
-		"drum",
-		"duck",
-		"ear",
-		"elephant",
-		"envelope",
-		"eye",
-		"fence",
-		"finger",
-		"fish",
-		"flag",
-		"flower",
-		"fly",
-		"foot",
-		"fork",
-		"fox",
-		"giraffe",
-		"glove",
-		"goat",
-		"gorilla",
-		"grapes",
-		"guitar",
-		"hammer",
-		"hand",
-		"harp",
-		"heart",
-		"helicopter",
-		"horse",
-		"kangaroo",
-		"key",
-		"kite",
-		"ladder",
-		"leaf",
-		"leg",
-		"lemon",
-		"lion",
-		"lobster",
-		"monkey",
-		"mouse",
-		"nail",
-		"nose",
-		"onion",
-		"orange",
-		"owl",
-		"peacock",
-		"pear",
-		"pen",
-		"pencil",
-		"penguin",
-		"pepper",
-		"piano",
-		"pig",
-		"pineapple",
-		"pipe",
-		"plane",
-		"pliers",
-		"pumpkin",
-		"rabbit",
-		"ring",
-		"rubber",
-		"ruler",
-		"saw",
-		"scissors",
-		"screw",
-		"screwdriver",
-		"seahorse",
-		"sheep",
-		"shirt",
-		"shoe",
-		"skirt",
-		"skunk",
-		"snail",
-		"snake",
-		"snowman",
-		"sock",
-		"spoon",
-		"squirrel",
-		"star",
-		"stool",
-		"sun",
-		"swan",
-		"swing",
-		"table",
-		"tie",
-		"tiger",
-		"toaster",
-		"toothbrush",
-		"tortoise",
-		"train",
-		"tree",
-		"trousers",
-		"trumpet",
-		"umbrella",
-		"vase",
-		"violin",
-		"watch",
-		"whistle",
-		"windmill",
-		"window",
-		"zebra"};
+    private static boolean mIsDialogRunning;
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		mDialogIntent = null;
-		mDataIntent = null;
+    public static String[] IMAGES = {
+            "anchor",
+            "ant",
+            "apple",
+            "arm",
+            "arrow",
+            "ashtray",
+            "ball",
+            "balloon",
+            "banana",
+            "bed",
+            "bell",
+            "belt",
+            "bench",
+            "bird",
+            "book",
+            "bottle",
+            "butterfly",
+            "button",
+            "camel",
+            "candle",
+            "car",
+            "carrot",
+            "cat",
+            "caterpillar",
+            "chisel",
+            "cigarette",
+            "clock",
+            "cloud",
+            "comb",
+            "cow",
+            "crown",
+            "desk",
+            "dog",
+            "door",
+            "dress",
+            "drum",
+            "duck",
+            "ear",
+            "elephant",
+            "envelope",
+            "eye",
+            "fence",
+            "finger",
+            "fish",
+            "flag",
+            "flower",
+            "fly",
+            "foot",
+            "fork",
+            "fox",
+            "giraffe",
+            "glove",
+            "goat",
+            "gorilla",
+            "grapes",
+            "guitar",
+            "hammer",
+            "hand",
+            "harp",
+            "heart",
+            "helicopter",
+            "horse",
+            "kangaroo",
+            "key",
+            "kite",
+            "ladder",
+            "leaf",
+            "leg",
+            "lemon",
+            "lion",
+            "lobster",
+            "monkey",
+            "mouse",
+            "nail",
+            "nose",
+            "onion",
+            "orange",
+            "owl",
+            "peacock",
+            "pear",
+            "pen",
+            "pencil",
+            "penguin",
+            "pepper",
+            "piano",
+            "pig",
+            "pineapple",
+            "pipe",
+            "plane",
+            "pliers",
+            "pumpkin",
+            "rabbit",
+            "ring",
+            "rubber",
+            "ruler",
+            "saw",
+            "scissors",
+            "screw",
+            "screwdriver",
+            "seahorse",
+            "sheep",
+            "shirt",
+            "shoe",
+            "skirt",
+            "skunk",
+            "snail",
+            "snake",
+            "snowman",
+            "sock",
+            "spoon",
+            "squirrel",
+            "star",
+            "stool",
+            "sun",
+            "swan",
+            "swing",
+            "table",
+            "tie",
+            "tiger",
+            "toaster",
+            "toothbrush",
+            "tortoise",
+            "train",
+            "tree",
+            "trousers",
+            "trumpet",
+            "umbrella",
+            "vase",
+            "violin",
+            "watch",
+            "whistle",
+            "windmill",
+            "window",
+            "zebra"};
 
-		mIsDialogRunning = false;
-	}
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mDialogIntent = null;
+        mDataIntent = null;
 
-	public static void startDialog() {
-		mIsDialogRunning = true;
-	}
+        mIsDialogRunning = false;
+    }
 
-	public static void stopDialog() {
-		mIsDialogRunning = false;
-	}
+    public static void startDialog() {
+        mIsDialogRunning = true;
+    }
 
-	public static boolean isDialogStarted() {
-		return mIsDialogRunning;
-	}
+    public static void stopDialog() {
+        mIsDialogRunning = false;
+    }
 
-	public static void startService(Context context, int secDialog) {
-		stopService(context);
-		
-		Intent backgroundDialogService = new Intent(context, AnnoyingService.class);
+    public static boolean isDialogStarted() {
+        return mIsDialogRunning;
+    }
 
-		Calendar cal = Calendar.getInstance();
-		long time = cal.getTimeInMillis();
-		
-		// A service can be stop by the system, so we need to schedule it...
-		mDialogIntent = PendingIntent.getService(context, 0,
-				backgroundDialogService, 0);
-		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-		// Start every DIALOG_TIME seconds.
-		alarmManager.set(AlarmManager.RTC_WAKEUP, time + secDialog * 1000, mDialogIntent);
-	}
-	
-	public static boolean stopService(Context context) {
-		if (mDialogIntent != null) {
-			AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-			alarmManager.cancel(mDialogIntent);
-			mDialogIntent = null;
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public static void startDataService(Context context, int secData) {
-		stopDataService(context);
-	
-		Intent backgroundDataService = new Intent(context, DataSenderService.class);
+    public static void startService(Context context, int secDialog) {
+        stopService(context);
 
-		Calendar cal = Calendar.getInstance();
-		long time = cal.getTimeInMillis();
-		
-		mDataIntent = PendingIntent.getService(context, 0, backgroundDataService, 0);
-		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-		
-		alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time + secData * 1000, secData * 1000, mDataIntent);
-	}
-	
-	public static boolean stopDataService(Context context) {
-		if (mDataIntent != null) {
-			AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-			alarmManager.cancel(mDataIntent);
-			mDataIntent = null;
-			return true;
-		} else {
-			return false;
-		}
-	}	
+        Intent backgroundDialogService = new Intent(context, AnnoyingService.class);
+
+        Calendar cal = Calendar.getInstance();
+        long time = cal.getTimeInMillis();
+
+        // A service can be stop by the system, so we need to schedule it...
+        mDialogIntent = PendingIntent.getService(context, 0,
+                backgroundDialogService, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        // Start every DIALOG_TIME seconds.
+        alarmManager.set(AlarmManager.RTC_WAKEUP, time + secDialog * 1000, mDialogIntent);
+    }
+
+    public static boolean stopService(Context context) {
+        if (mDialogIntent != null) {
+            AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            alarmManager.cancel(mDialogIntent);
+            mDialogIntent = null;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void startDataService(Context context, int secData) {
+        stopDataService(context);
+
+        Intent backgroundDataService = new Intent(context, DataSenderService.class);
+
+        Calendar cal = Calendar.getInstance();
+        long time = cal.getTimeInMillis();
+
+        mDataIntent = PendingIntent.getService(context, 0, backgroundDataService, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time + secData * 1000, secData * 1000, mDataIntent);
+    }
+
+    public static boolean stopDataService(Context context) {
+        if (mDataIntent != null) {
+            AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            alarmManager.cancel(mDataIntent);
+            mDataIntent = null;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void launchStatusIcon(Context context) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setOngoing(true)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle(context.getString(R.string.app_name))
+                        .setContentText(context.getString(R.string.app_text));
+        // Creates an explicit intent for an Activity in your app
+
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(Common.APP_NOTIF, mBuilder.build());
+    }
 }
--- src/edu/hci/annoyingapp/activities/MainActivity.java
+++ src/edu/hci/annoyingapp/activities/MainActivity.java
@@ -1,6 +1,5 @@
 package edu.hci.annoyingapp.activities;
 
-import android.app.NotificationManager;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -10,10 +9,10 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
 import edu.hci.annoyingapp.AnnoyingApplication;
 import edu.hci.annoyingapp.R;
 import edu.hci.annoyingapp.protocol.Receivers;
@@ -21,124 +20,108 @@ import edu.hci.annoyingapp.utils.Common;
 
 public class MainActivity extends FragmentActivity implements OnClickListener {
 
-	private static final String TAG = MainActivity.class.getSimpleName();
-	private static final boolean DEBUG_MODE = AnnoyingApplication.DEBUG_MODE;
-	
-	private ProgressDialog mDialog;
-	private String mSurveyUrl;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final boolean DEBUG_MODE = AnnoyingApplication.DEBUG_MODE;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    private ProgressDialog mDialog;
+    private String mSurveyUrl;
 
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		SharedPreferences settings = getSharedPreferences(
-				Common.PREFS_NAME, 0);
-		
-		int bigInterval = settings.getInt(Common.PREF_BIG_INTERVAL,
-				-1);
+        setContentView(R.layout.activity_main);
 
-		boolean isRunning = settings.getBoolean(
-				Common.PREF_IS_SERVICE_RUNNING,
-				false);
-		
-		int dataInterval = settings.getInt(Common.PREF_DATA_INTERVAL,
-				-1);
-		
-		mSurveyUrl = settings.getString(Common.PREF_FIRST_SURVEY, null);
-		
-		String token = settings.getString(Common.PREF_TOKEN, null);
-		
-		mSurveyUrl += token;
-		
-		boolean firstTime = settings.getBoolean(Common.PREF_FIRST_TIME, true);
-		
-		if(mSurveyUrl != null && firstTime) {
-			SharedPreferences.Editor editor = settings.edit();
-			editor.putBoolean(Common.PREF_FIRST_TIME, false);
-			editor.commit();
-			launchDemoSurvey();
-		}
-		
-		Button survey = (Button) findViewById(R.id.activity_main_demo_survey);
-		survey.setOnClickListener(this);
-	
-		if(isRunning) {
-			AnnoyingApplication.startService(this, bigInterval);
-		}
-		
-		launchStatusIcon();
-		
-		AnnoyingApplication.startDataService(this, dataInterval);
-		
-		registerReceiver(mHandleUnregistrationReceiver,new IntentFilter(Receivers.UNREGISTERED));
-	}
+        SharedPreferences settings = getSharedPreferences(
+                Common.PREFS_NAME, 0);
 
-	@Override
-	protected void onStop() {
+        int bigInterval = settings.getInt(Common.PREF_BIG_INTERVAL,
+                -1);
 
-		super.onStop();
-		// Little trick so that activity is not on background when dialog
-		// shows up.
-		finish();
-	}
-	
-	@Override
-	protected void onDestroy() {
-		unregisterReceiver(mHandleUnregistrationReceiver);
-		super.onDestroy();
-	}
+        boolean isRunning = settings.getBoolean(
+                Common.PREF_IS_SERVICE_RUNNING,
+                false);
 
-	private final BroadcastReceiver mHandleUnregistrationReceiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			SharedPreferences settings = context.getSharedPreferences(
-					Common.PREFS_NAME, 0);
-			SharedPreferences.Editor editor = settings.edit();
-			editor.putBoolean(Common.PREF_IS_SERVICE_RUNNING, false);
-			editor.commit();
-			
-			AnnoyingApplication.stopService(context);
-			
-			Intent i = new Intent(MainActivity.this, LoginActivity.class);
-			startActivity(i);
-			finish();
-			mDialog.dismiss();
-		}
-	};
-	
-	private void launchStatusIcon() {
-		NotificationCompat.Builder mBuilder =
-		        new NotificationCompat.Builder(this)
-				.setOngoing(true)
-		        .setSmallIcon(R.drawable.ic_launcher)
-		        .setContentTitle(getString(R.string.app_name))
-		        .setContentText(getString(R.string.app_text));
-		// Creates an explicit intent for an Activity in your app
-		
-		
-		NotificationManager mNotificationManager =
-		    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		// mId allows you to update the notification later on.
-		mNotificationManager.notify(Common.APP_NOTIF, mBuilder.build());
-	}
-	
-	@Override
-	public void onClick(View v) {
-		if(v.getId() == R.id.activity_main_demo_survey) {
-			launchDemoSurvey();
-		}
-	}
-	
-	private void launchDemoSurvey() {
-		
-		Intent i = new Intent(Intent.ACTION_VIEW);
-		i.setData(Uri.parse(mSurveyUrl));
-		startActivity(i);
-		
-		//Intent i = new Intent(this, SurveyActivity.class);
-		//i.putExtra(SurveyActivity.EXTRA_SURVEY, mSurveyUrl);
-		//startActivity(i);
-	}
+        int dataInterval = settings.getInt(Common.PREF_DATA_INTERVAL,
+                -1);
+
+        mSurveyUrl = settings.getString(Common.PREF_FIRST_SURVEY, null);
+
+        String token = settings.getString(Common.PREF_TOKEN, null);
+
+        mSurveyUrl += token;
+
+        boolean firstTime = settings.getBoolean(Common.PREF_FIRST_TIME, true);
+
+        if (mSurveyUrl != null && firstTime) {
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putBoolean(Common.PREF_FIRST_TIME, false);
+            editor.commit();
+            launchDemoSurvey();
+        }
+
+        Button survey = (Button) findViewById(R.id.activity_main_demo_survey);
+        survey.setOnClickListener(this);
+
+        if (isRunning) {
+            AnnoyingApplication.startService(this, bigInterval);
+        }
+
+        AnnoyingApplication.launchStatusIcon(this);
+
+        AnnoyingApplication.startDataService(this, dataInterval);
+
+        registerReceiver(mHandleUnregistrationReceiver, new IntentFilter(Receivers.UNREGISTERED));
+    }
+
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+        // Little trick so that activity is not on background when dialog
+        // shows up.
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mHandleUnregistrationReceiver);
+        super.onDestroy();
+    }
+
+    private final BroadcastReceiver mHandleUnregistrationReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            SharedPreferences settings = context.getSharedPreferences(
+                    Common.PREFS_NAME, 0);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putBoolean(Common.PREF_IS_SERVICE_RUNNING, false);
+            editor.commit();
+
+            AnnoyingApplication.stopService(context);
+
+            Intent i = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(i);
+            finish();
+            mDialog.dismiss();
+        }
+    };
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.activity_main_demo_survey) {
+            launchDemoSurvey();
+        }
+    }
+
+    private void launchDemoSurvey() {
+
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(mSurveyUrl));
+        startActivity(i);
+
+        //Intent i = new Intent(this, SurveyActivity.class);
+        //i.putExtra(SurveyActivity.EXTRA_SURVEY, mSurveyUrl);
+        //startActivity(i);
+    }
 }
